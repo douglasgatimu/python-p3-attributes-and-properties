d5ddcf385e20cda1942930829ca3cb33771958c0
Douglas Gatimu <douglasgatimu@gmail.com>
2025-07-13 00:58:30 +0300

    Added classes Dog and Person.

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='John Doe', job='Jobless'):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if name and isinstance(name, str) and (1 <= len(name) <= 25):
+            # print(f"Setting name to { name }.")
+            name = name.title()
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")        
+        
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            # print(f"Setting job to { job }.")
+            self._job = job
+
+        else:
+            print("Job must be in list of approved jobs.")
+        
+
+    job = property(get_job, set_job)    
+
